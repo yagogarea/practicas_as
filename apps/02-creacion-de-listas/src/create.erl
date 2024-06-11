2d10b17c2c241d2d7d4f08dfb151f6c800a85bde
yagogarea <yagogareacidre@gmail.com>
2024-06-11 12:13:48 +0200

    feat: implements list creation operations

--- apps/02-creacion-de-listas/src/create.erl
+++ apps/02-creacion-de-listas/src/create.erl
@@ -8,13 +8,30 @@
 %% @spec create(Value :: pos_integer()) -> [pos_integer()]
 %% @end
 %%--------------------------------------------------------------------
-create(_Value) ->
-    erlang:throw(not_implemented).
+create(N) when is_number(N)->
+    create(N,[]);
+create(_) ->
+    erlang:throw("NAN").
+
+create(0, List) ->
+    List;
+create(N, List) ->
+    create(N-1, [N|List]).
 
 %%--------------------------------------------------------------------
 %% @doc Reverse create function.
 %% @spec reverse_create(Value :: pos_integer()) -> [pos_integer()]
 %% @end
 %%--------------------------------------------------------------------
-reverse_create(_Value) ->
-    erlang:throw(not_implemented).
+reverse_create(N) when is_number(N)->
+    List = create(N),
+    RevList = rev_list(List,[]),
+    RevList; 
+reverse_create(_) ->
+    erlang:throw("NAN").
+
+%%Auxiliar function
+rev_list([],Acc) ->
+    Acc;
+rev_list([H|T], Acc) ->
+    rev_list(T, [H|Acc]).
