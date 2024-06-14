d9cd3e2657368548440a3e0b3cc4174a407b2970
yagogarea <yagogareacidre@gmail.com>
2024-06-14 13:34:02 +0200

    fix: pr comments

--- apps/05-algoritmos-de-ordenacion/src/sorting.erl
+++ apps/05-algoritmos-de-ordenacion/src/sorting.erl
@@ -12,7 +12,7 @@ quicksort([]) ->
     [];
 quicksort([X]) ->
     [X];
-quicksort([H|T])  ->
+quicksort([H | T])  ->
    List1 = [X || X <- T, X =< H],
    List2 = [X || X <- T, X > H],
    quicksort(List1) ++ [H] ++  quicksort(List2);
@@ -30,14 +30,16 @@ mergesort([]) ->
 mergesort([X]) ->
     quicksort([X]);
 mergesort([X1, X2]) ->
-    quicksort([X1,X2]);
+    quicksort([X1, X2]);
 mergesort(List) when is_list(List) ->
     {List1, List2} = lists:split(lsize(List) div 2, List),
     quicksort(mergesort(List1) ++ mergesort(List2));
 mergesort(_NotAList) ->
     erlang:throw("Not a list").
 
-%% Auxiliar function 
+%%%-----------------------------------------------------------------------------
+%%% INTERNAL FUNCTIONS
+%%%-----------------------------------------------------------------------------
 lsize([]) ->
     0;
 lsize([_|T]) ->
