c9ba5bed01f3b7d49e5a2eff23f0911a733d26f8
yagogarea <yagogareacidre@gmail.com>
2024-06-14 11:59:59 +0200

    feat: implements process crossring

--- apps/09-anillo-cruzado-de-procesos/src/crossring.erl
+++ apps/09-anillo-cruzado-de-procesos/src/crossring.erl
@@ -2,7 +2,7 @@
 
 % Public API
 -export([start/3]).
--export([init/5, init/6, loop/4]).
+-export([init/5, init_cross/4, loop/3, loop_cross/3, rec_star/1]).
 
 %%--------------------------------------------------------------------
 %% @doc Start function.
@@ -11,84 +11,133 @@
 %%--------------------------------------------------------------------
 
 start(ProcNum, MsgNum, Message) ->
-    Middle = (ProcNum - 1) div 2,
-    register(master, spawn(?MODULE, init, [ProcNum - 1, Middle, MsgNum, Message, self()])),
-    wait(ProcNum).
+    register(cross, spawn(?MODULE, init_cross, [ProcNum - 1, MsgNum, Message, self()])),
+    wait_procs(ProcNum).
 
-wait(0) ->
-    ok;
-wait(ProcNum) ->
+
+init_cross(0, MsgNum, Message, From) -> 
+    self() ! {MsgNum - 1, Message, none},
+    io:format("~p~n",[Message]),
+    loop_cross(self(), none, From);
+
+init_cross(ProcNum, MsgNum, Message, From) ->
+    P1 = ProcNum div 2,
+    P2 = ProcNum - P1,
+    create_rings(P1, P2, MsgNum, Message, From).
+
+create_rings(0, 1, MsgNum, Message, From) -> 
+    Next = spawn(?MODULE, init, [0, MsgNum, Message, self(), From]),
+    rec_star(1),
+    Next ! {MsgNum - 1, Message, one},
+    io:format("~p~n",[Message]),
+    loop_cross(Next, one, From);
+    
+create_rings(P1, P2, MsgNum, Message, From) ->
+    Next = spawn(?MODULE, init, [P1 - 1, MsgNum, Message, self(), From]),
+    Next2 = spawn(?MODULE, init, [P2 - 1, MsgNum, Message, self(), From]),
+    rec_star(2),
+    Next ! {MsgNum - 1, Message, first},
+    io:format("~p~n",[Message]),
+    loop_cross(Next, Next2, From).
+
+
+init(0, _MsgNum, _Message, Cross, From) ->
+    Cross ! readyone,
+    loop(Cross, Cross, From);
+init(ProcNum, MsgNum, Message, Cross, From) ->
+    Next = spawn(?MODULE, init, [ProcNum - 1, MsgNum, Message, Cross, From]), 
+    loop(Next, Cross, From).
+
+
+loop_cross(Cross, none, From) -> % 1 proc
     receive
-        close ->
-            wait(ProcNum - 1)
+        {0, _Message, _State} ->
+            Cross ! stop,
+            loop_cross(Cross, none, From);
+        {MsgNum, Message, State} ->
+            Cross ! {MsgNum - 1, Message, State},
+            io:format("~p~n",[Message]),
+            loop_cross(Cross, none, From);
+        stop ->
+            From ! close
+    end;
+
+loop_cross(Next, one, From) -> % 2 proc
+    receive
+        {0, _Message, _State} ->
+            self() ! fin,
+            loop_cross(Next, one, From);
+        {MsgNum, Message, State} ->
+            Next ! {MsgNum - 1, Message, State},
+            io:format("~p~n",[Message]),
+            loop_cross(Next, one, From);
+        fin ->
+            Next ! stop,
+            From ! close
+    end;
+
+loop_cross(Next1, Next2, From) ->
+    receive
+        {0, _Message, _State} ->
+            self() ! fin,
+            loop_cross(Next1, Next2, From);
+        {MsgNum, Message, first} ->
+            Next1 ! {MsgNum - 1, Message, second},
+            io:format("~p~n",[Message]),
+            loop_cross(Next1, Next2, From);
+        {MsgNum, Message, second} ->
+            Next2 ! {MsgNum - 1, Message, first},
+            io:format("~p~n",[Message]),
+            loop_cross(Next1, Next2, From);
+        fin ->
+            Next1 ! stop,
+            Next2 ! stop,
+            From ! close
+        end.
+
+loop(Cross, Cross, From) ->
+    receive
+        {0, _Message, _State} ->
+            Cross ! fin,
+            loop(Cross, Cross, From);
+        {MsgNum, Message, State} ->
+            Cross ! {MsgNum - 1, Message, State},
+            loop(Cross, Cross, From);
+        stop ->
+            Cross ! stop,
+            From ! close
+    end;
+
+loop(Next, Cross, From) ->
+    receive
+        {0, _Message, _State} ->
+            Cross ! fin,
+            loop(Next, Cross, From);
+        {MsgNum, Message, State} ->
+            Next ! {MsgNum - 1, Message, State},
+            io:format("~p~n",[Message]),
+            loop(Next, Cross, From);
+        stop ->
+            Next ! stop,
+            From ! close
     end.
 
-init(ProcNum, Middle, MsgNum, Message, From) ->
-    Master = whereis(master),
-    init(ProcNum, Middle, MsgNum, Message, From, Master).
+% Auxiliar functions
 
-init(0, Middle, MsgNum, Message, From, Master) when is_pid(Middle) ->
-    io:format("~p  ~n", [Message]),
-    master ! {MsgNum - 1, Message, schild},
-    loop(self(), Master, From, Master);
+rec_star(0) ->
+    ok;
 
-init(0, _Middle, MsgNum, Message, From, Master) ->
-    io:format("~p ~n", [Message]),
-    master ! {MsgNum - 1, Message, self()},
-    loop(self(), Master, From, Master);
-
-init(ProcNum, ProcNum, MsgNum, Message, From, Master) ->
-    register(schild, spawn(?MODULE, init, [ProcNum - 1, self(), MsgNum, Message, From, Master])),
-    loop(self(), Master, From, Master);
-
-init(ProcNum, Middle, MsgNum, Message, From, Master) ->
-    Next = spawn(?MODULE, init, [ProcNum - 1, Middle, MsgNum, Message, From, Master]),
-    loop(self(), Next, From, Master).
+rec_star(Nrec) ->
+    receive
+        readyone ->
+            rec_star(Nrec - 1)
+    end.
 
 
-loop(Master, Next, From, Master) ->
-    receive 
-        {0, _Message, Schild} ->
-            Master ! {close, Schild},
-            loop(Master, Next, From, Master);
-        {MsgNum, Message, Schild} ->
-            io:format("~p ~n", [Message]),
-            Next ! {MsgNum - 1, Message, Next},
-            loop(Master, Schild, From, Master);
-        {close, Schild} ->
-            Next ! stop,
-            case Schild of
-                Master ->
-                    ok;
-                Next ->
-                    ok;
-                _ ->
-                    Schild ! stop
-            end,
-            From ! close
-    end;
-loop(Me, Master, From, Master) ->
-    receive 
-        {0, _Message, Schild} ->
-            master ! {close, Schild},
-            loop(Me, Master, From, Master);
-        {MsgNum, Message, Schild} ->
-            io:format("~p ~n", [Message]),
-            Master ! {MsgNum - 1, Message, Schild},
-            loop(Me, Master, From, Master);
-        stop ->
-            From ! close
-    end;
-loop(Me, Next, From, Master) ->
-    receive 
-        {0, _Message, Schild} ->
-            master ! {close, Schild},
-            loop(Me, Next, From, Master);
-        {MsgNum, Message, Schild} ->
-            io:format("~p ~n", [Message]),
-            Next ! {MsgNum - 1, Message, Schild},
-            loop(Me, Next, From, Master);
-        stop ->
-            Next ! stop,
-            From ! close
+wait_procs(0) ->
+    ok;
+wait_procs(ProcNum) ->
+    receive
+        close ->
+            wait_procs(ProcNum - 1)
     end.
