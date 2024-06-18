7510977da9f2d8536e64f6f57e0d1bb90c3cc457
yagogarea <127434255+yagogarea@users.noreply.github.com>
2024-06-18 10:50:54 +0200

    fix: issue format

--- apps/04-manipulacion-de-listas/src/manipulating.erl
+++ apps/04-manipulacion-de-listas/src/manipulating.erl
@@ -9,9 +9,7 @@
 %% @end
 %%--------------------------------------------------------------------
 filter(List, N) when is_integer(N) ->
-    filter(List, N, []);
-filter(_List, _N) ->
-    erlang:throw("NAN").
+    filter(List, N, []).
 
 %%--------------------------------------------------------------------
 %% @doc Reverse function.
@@ -35,7 +33,6 @@ concatenate(List)->
 %% @spec flatten(DeepList :: [integer() | list()]) -> [integer()]
 %% @end
 %%--------------------------------------------------------------------
-
 flatten(List) ->
     Result = flatten(List, []),
     reverse(Result).
@@ -43,20 +40,15 @@ flatten(List) ->
 %%%-----------------------------------------------------------------------------
 %%% INTERNAL FUNCTIONS
 %%%-----------------------------------------------------------------------------
-
 aux([], Acc) ->
     Acc;
 aux([H | T], Acc) ->
     aux(T, [H | Acc]).
 
-
 concatenate([], Acc) ->
     Acc;
 concatenate([H | T], Acc) when is_list(H)->
-    concatenate(T, aux(H, Acc));
-concatenate(_ListOfLists, _) ->
-    erlang:throw("Not a list of lists").
-
+    concatenate(T, aux(H, Acc)).
 
 filter([], _N, Acc) ->
     reverse(Acc);
@@ -65,20 +57,14 @@ filter([H | T], N, Acc) when H > N ->
 filter([H | T], N, Acc) ->
     filter(T, N, [H | Acc]).
 
-
 flatten([], Acc) ->
     Acc;
 flatten([H | T], Acc) when is_list(H)->
     flatten(T, flatten(H, Acc));
 flatten([H | T], Acc) ->
-    flatten(T, [H | Acc]);
-flatten(_List, _) ->
-    erlang:throw("Not a list").
-
+    flatten(T, [H | Acc]).
 
 reverse([], Acc) ->
     Acc;
 reverse([H | T], Acc) ->
-   reverse(T, [H | Acc]);
-reverse(_, _) ->
-    erlang:throw("Not a list").
+   reverse(T, [H | Acc]).
