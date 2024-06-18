bf9e324f661b1d5feeced1d74ba38cc7160014a2
yagogarea <127434255+yagogarea@users.noreply.github.com>
2024-06-18 10:26:01 +0200

    Feat/04 list manipulation (#7)
    * feat: implement list manipulation operations
    
    * fix: pr comments
    
    * fix: ci wrong test
    
    * fix: tabulations and change ++ operator

--- apps/04-manipulacion-de-listas/src/manipulating.erl
+++ apps/04-manipulacion-de-listas/src/manipulating.erl
@@ -8,29 +8,77 @@
 %% @spec filter(List :: [integer()], N :: integer()) -> [integer()]
 %% @end
 %%--------------------------------------------------------------------
+filter(List, N) when is_integer(N) ->
+    filter(List, N, []);
 filter(_List, _N) ->
-    erlang:throw(not_implemented).
+    erlang:throw("NAN").
 
 %%--------------------------------------------------------------------
 %% @doc Reverse function.
 %% @spec reverse(List :: [integer()]) -> [integer()]
 %% @end
 %%--------------------------------------------------------------------
-reverse(_List) ->
-    erlang:throw(not_implemented).
+reverse(List) ->
+    reverse(List, []).
 
 %%--------------------------------------------------------------------
 %% @doc Concatenate function.
 %% @spec concatenate(ListOfLists :: [[integer()]]) -> [integer()]
 %% @end
 %%--------------------------------------------------------------------
-concatenate(_ListOfLists) ->
-    erlang:throw(not_implemented).
-
+concatenate(List)->
+    Result = concatenate(List, []),
+    reverse(Result).
+        
 %%--------------------------------------------------------------------
 %% @doc Flatten function.
 %% @spec flatten(DeepList :: [integer() | list()]) -> [integer()]
 %% @end
 %%--------------------------------------------------------------------
-flatten(_DeepList) ->
-    erlang:throw(not_implemented).
+
+flatten(List) ->
+    Result = flatten(List, []),
+    reverse(Result).
+
+%%%-----------------------------------------------------------------------------
+%%% INTERNAL FUNCTIONS
+%%%-----------------------------------------------------------------------------
+
+aux([], Acc) ->
+    Acc;
+aux([H | T], Acc) ->
+    aux(T, [H | Acc]).
+
+
+concatenate([], Acc) ->
+    Acc;
+concatenate([H | T], Acc) when is_list(H)->
+    concatenate(T, aux(H, Acc));
+concatenate(_ListOfLists, _) ->
+    erlang:throw("Not a list of lists").
+
+
+filter([], _N, Acc) ->
+    reverse(Acc);
+filter([H | T], N, Acc) when H > N ->
+    filter(T, N, Acc);
+filter([H | T], N, Acc) ->
+    filter(T, N, [H | Acc]).
+
+
+flatten([], Acc) ->
+    Acc;
+flatten([H | T], Acc) when is_list(H)->
+    flatten(T, flatten(H, Acc));
+flatten([H | T], Acc) ->
+    flatten(T, [H | Acc]);
+flatten(_List, _) ->
+    erlang:throw("Not a list").
+
+
+reverse([], Acc) ->
+    Acc;
+reverse([H | T], Acc) ->
+   reverse(T, [H | Acc]);
+reverse(_, _) ->
+    erlang:throw("Not a list").
