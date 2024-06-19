e73cd07b66bf45c5f635e90824b1a5021efdd89f
yagogarea <127434255+yagogarea@users.noreply.github.com>
2024-06-19 13:20:59 +0200

    feat(05): sorting algorithms  (#8)
    * feat: implement list manipulation operations
    
    * feat: implement list sorting operations
    
    * fix: pr comments
    
    * update ci.yml
    
    * fix: add newline at end of file
    
    * fix: update base case in mergesort
    
    * fix: bad format
    
    * fix: missing final dots
    
    * fix: final line break
    
    * fix: missing space in lsize

--- apps/05-algoritmos-de-ordenacion/src/sorting.erl
+++ apps/05-algoritmos-de-ordenacion/src/sorting.erl
@@ -8,13 +8,34 @@
 %% @spec quicksort(List :: [integer()]) -> [integer()]
 %% @end
 %%--------------------------------------------------------------------
-quicksort(_List) ->
-    erlang:throw(not_implemented).
+quicksort([]) ->
+    [];
+quicksort([X]) ->
+    [X];
+quicksort([H | T])  ->
+   List1 = [X || X <- T, X =< H],
+   List2 = [X || X <- T, X > H],
+   quicksort(List1) ++ [H] ++  quicksort(List2).
 
 %%--------------------------------------------------------------------
 %% @doc Mergesort function.
 %% @spec mergesort(List :: [integer()]) -> [integer()]
 %% @end
 %%--------------------------------------------------------------------
-mergesort(_List) ->
-    erlang:throw(not_implemented).
+mergesort([]) ->
+    [];
+mergesort([X]) ->
+    [X];
+mergesort([X1, X2]) ->
+    quicksort([X1, X2]);
+mergesort(List) when is_list(List) ->
+    {List1, List2} = lists:split(lsize(List) div 2, List),
+    quicksort(mergesort(List1) ++ mergesort(List2)).
+
+%%%-----------------------------------------------------------------------------
+%%% INTERNAL FUNCTIONS
+%%%-----------------------------------------------------------------------------
+lsize([]) ->
+    0;
+lsize([_ | T]) ->
+    1 + lsize(T).
