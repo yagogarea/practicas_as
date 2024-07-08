bcc87587d79b8c0e8a4e4286374df620b5ebc2c7
yagogarea <127434255+yagogarea@users.noreply.github.com>
2024-07-08 09:31:23 +0200

    feat(07): echo server (#10)
    * feat: implements echo server
    
    * fix: pr comments
    
    * fix: add space
    
    * fix: ci echo test
    
    * fix: bad format
    
    * fix: typo
    
    * fix: ci bad rebase
    
    * fix: unnecessary semicolon
    
    * fix: typo in ci

--- apps/07-servidor-echo/src/echo.erl
+++ apps/07-servidor-echo/src/echo.erl
@@ -3,13 +3,16 @@
 % Public API
 -export([start/0, stop/0, print/1]).
 
+-export([loop/0]).
+
 %%--------------------------------------------------------------------
 %% @doc Start function.
 %% @spec start() -> ok
 %% @end
 %%--------------------------------------------------------------------
 start() ->
-    erlang:throw(not_implemented).
+    register(?MODULE, spawn(?MODULE, loop, [])),
+    ok.
 
 %%--------------------------------------------------------------------
 %% @doc Stop function.
@@ -17,12 +20,29 @@ start() ->
 %% @end
 %%--------------------------------------------------------------------
 stop() ->
-    erlang:throw(not_implemented).
+    ?MODULE ! {stop, self()},
+    receive
+        stopped ->
+            ok
+    end.
 
 %%--------------------------------------------------------------------
 %% @doc Print function.
 %% @spec print(Term :: term()) -> ok
 %% @end
 %%--------------------------------------------------------------------
-print(_Term) ->
-    erlang:throw(not_implemented).
+print(Term) ->
+    ?MODULE ! {print, Term},
+    ok.
+
+%%%-------------------------------------------------------------------
+%%% INTERNAL EXPORTS
+%%%-------------------------------------------------------------------
+loop() ->
+    receive
+        {print, Term} ->
+            io:format("~p~n", [Term]),
+            loop();
+        {stop, From} ->
+            From ! stopped
+    end.
